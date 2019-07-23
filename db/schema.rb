80fdffe3c454d2cf0cdfc9fe2f0811438adefe01
etrex <et284vu065k3@gmail.com>
2019-07-23 21:06:42 +0800

    change db to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_07_21_085354) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "todos", force: :cascade do |t|
     t.string "name"
     t.string "desc"
