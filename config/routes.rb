9492ac2a6d6a39abc05c5bcaed952dc47fb5c955
etrex kuo <et284vu065k3@gmail.com>
2021-06-06 16:09:55 +0800

    新增串接資訊

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Rails.application.routes.draw do
-  root to: 'home#index'
+  root to: "home#index"
+
+  get "follow", to: "home#follow"
+  get "follow", to: "home#follow"
+  get "join", to: "home#join"
 
   resources :todos
 
-  get "index", to: 'home#index'
-  get "menu", to: 'home#menu'
-  get "目錄", to: 'home#menu'
+  get "index", to: "home#index"
+  get "menu", to: "home#menu"
+  get "目錄", to: "home#menu"
 
-  get '*location天氣', to: 'weather#show'
-  get '(*location)天氣(*other)', to: 'weather#show'
+  get "*location天氣", to: "weather#show"
+  get "(*location)天氣(*other)", to: "weather#show"
 
-  get 'test', to: 'home#test'
-  get 'profile', to: 'home#profile'
+  get "test", to: "home#test"
+  get "profile", to: "home#profile"
 end
